5ed3db528efb9f91b3cb7e05d3e8735e6a6a03bb
Michael Bykov <m.bykov@gmail.com>
2021-07-11 21:02:45 +0300

    i

--- index.js
+++ index.js
@@ -5,6 +5,49 @@ const path = require("path")
 const JSON5 = require('json5')
 
 
+let config = readConf()
+log('_CONF', config)
+
+let heapPath = path.resolve(__dirname, config.heap)
+log('_heapPath', heapPath)
+
+let cards = getCards(heapPath)
+log('_CARDS_', cards.length)
+
+// =========== HOME
+process.env['HOME']
+
+let card = {
+  current: 'a',
+
+  show() {
+    let desc = this.current[this.step]
+    if (!desc) {
+      log('_RANDOM-CARD')
+      this.random()
+    } else {
+      log('_DESC:', desc)
+    }
+  },
+
+  nextStep() {
+    this.step += 1
+    this.show()
+  },
+
+  prevStep() {
+    this.step -= 1
+    this.show()
+  },
+
+  random() {
+    let idx = getRandomInt(cards.length)
+    this.current = cards[idx]
+    this.step = 0
+    this.show()
+  }
+}
+
 const readline = require('readline');
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
@@ -12,37 +55,75 @@ process.stdin.on('keypress', (str, key) => {
   if (key.ctrl && key.name === 'c') {
     process.exit();
   } else {
-    // console.log(`You pressed the "${str}" key`);
-    // console.log();
-    // console.log('_KEY', key);
-    // console.log();
-
     if (key.name == 'down' && key.shift) {
       console.log('_Shift+Down');
     } else if (key.name == 'down') {
       console.log('_Down');
+      card.random()
     } else if (key.name == 'up') {
       console.log('_Up');
+      card.random()
     } else if (key.name == 'left') {
       console.log('_Left');
+      card.prevStep()
     } else if (key.name == 'right') {
       console.log('_Right');
+      card.nextStep()
     }
-
   }
 });
+
+function showCard(step) {
+  let idx = getRandomInt(cards.length)
+  let card = cards[idx]
+  if (step) log('_CARD', card[1])
+  else log('_CARD', card[0])
+}
+
+card.random()
 console.log('Press any key...');
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
-// let config = readConf()
-// log('_CONF', config)
 
-// let heapPath = path.resolve(__dirname, config.heap)
-// log('_heapPath', heapPath)
+function getCards(heapPath) {
+  try {
+    heapPath = path.resolve(heapPath, 'Greek/lushing-01.md')
+    let str = fse.readFileSync(heapPath).toString().trim()
+    let rows = str.trim().split('\n')
+    let cards = [], card
+    let comm = ''
+    rows.forEach(row=> {
+      if (!row) return
+      let tmp = row.slice(0,2)
+      if (tmp == '# ') {
+        comm = ''
+        return
+      } else if (tmp == '##') {
+        comm = row
+      }  else {
+        card = row.trim().split(' = ')
+        if (comm) card.push(comm)
+      }
+      cards.push(card)
+    })
+    return cards
+  } catch (err) {
+    log('_ERR CARDS')
+  }
+}
 
-// let cards = getCards(heapPath)
-// cards = cards.slice(-5)
-// log('_CARDS', cards)
+function readConf() {
+  try {
+    let str = fse.readFileSync('./config.json').toString()
+    let conf = JSON5.parse(str)
+    return conf
+  } catch (err) {
+    log('_ERR CONF')
+  }
+}
 
 // inquirer
 //   .prompt([
@@ -67,40 +148,3 @@ console.log('Press any key...');
 //       console.log('_ERR');
 //     }
 //   });
-
-// function getCards(heapPath) {
-//   try {
-//     heapPath = path.resolve(heapPath, 'Greek/lushing-01.md')
-//     let str = fse.readFileSync(heapPath).toString().trim()
-//     let rows = str.trim().split('\n')
-//     let cards = [], card
-//     let comm = ''
-//     rows.forEach(row=> {
-//       if (!row) return
-//       let tmp = row.slice(0,2)
-//       if (tmp == '# ') {
-//         comm = ''
-//         return
-//       } else if (tmp == '##') {
-//         comm = row
-//       }  else {
-//         card = row.trim().split(' = ')
-//         if (comm) card.push(comm)
-//       }
-//       cards.push(card)
-//     })
-//     return cards
-//   } catch (err) {
-//     log('_ERR CARDS')
-//   }
-// }
-
-// function readConf() {
-//   try {
-//     let str = fse.readFileSync('./config.json').toString()
-//     let conf = JSON5.parse(str)
-//     return conf
-//   } catch (err) {
-//     log('_ERR CONF')
-//   }
-// }
